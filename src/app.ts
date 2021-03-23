ddc1c8c8e08c40876aba55c1a087783f28792bac
Mert <mertturkmenoglu99@gmail.com>
2021-03-23 22:22:20 +0300

    Add Mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
 import express from "express";
-import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServer } from "apollo-server-express";
+import mongoose from "mongoose";
 
 import { resolvers } from "./resolvers";
 import { typeDefs } from "./typeDefs";
 
 const app = express();
+
+const MONGO_URI = "mongodb://localhost:27017/graphql-test-db";
+const MONGOOSE_OPTIONS: mongoose.ConnectOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(MONGO_URI, MONGOOSE_OPTIONS, () => {
+  console.log("Connected to MongoDB");
+});
 
 const server = new ApolloServer({
   typeDefs,
