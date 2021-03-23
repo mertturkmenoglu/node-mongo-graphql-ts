165512d86ba0b4823b84737ef0a236b9a9fd9e86
Mert <mertturkmenoglu99@gmail.com>
2021-03-23 22:16:10 +0300

    Add GraphQL

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,18 @@
 import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+
+import { resolvers } from "./resolvers";
+import { typeDefs } from "./typeDefs";
 
 const app = express();
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
+
 app.listen(4000, () => {
-  console.log(`Server started listening on port 4000: http://localhost:4000/`);
+  console.log(`Server started listening on port 4000: http://localhost:4000${server.graphqlPath}`);
 });
